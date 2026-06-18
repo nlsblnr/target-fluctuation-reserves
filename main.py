294694d5e47b6627aa32c44aa573c04f8a88b0b2
nlsblnr <nils.blunier@gmail.com>
2026-06-18 07:05:12 +0200

    finalized stock path simulation; simulates n runs and plots them together for testing

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ We will have to agree on an initial asset portfolio and initial liabilities. For
 
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
-sim_runs = 100
+sim_runs = 20
 observed_time = 1
 delta_t = 1/365
 
@@ -28,9 +29,40 @@ class Asset:
         self.sigma = sigma
         self.mu = mu
         self.invested_capital = invested_capital
+        self.initial_capital = invested_capital
+        self.brownian_motion_process = []
 
-    def calculate_next_price(self, invested_capital):
-        pass
+    def calculate_next_price(self, run_index, invested_capital):
+        
+        time_now = run_index * delta_t
+        
+        if run_index == 0:
+            self.brownian_motion_process.append(0)
+            new_invested_capital = self.initial_capital
+        else:
+            dW = np.random.normal(loc=0, scale=np.sqrt(delta_t))
+            self.brownian_motion_process.append(self.brownian_motion_process[-1] + dW)
+            
+            drift = self.mu - self.sigma**2/2
+            
+            new_invested_capital = self.initial_capital*np.exp(drift*time_now + self.sigma*self.brownian_motion_process[run_index])
+            
+        self.invested_capital = float(new_invested_capital)
+        return self.invested_capital
+
 
 # define liabilities as an amount constant over time
 liabilities = 7E08
+
+for a in range(sim_runs):
+    
+    stock_price = []
+    stocks = Asset(0.9, 0.1, 1E06)
+    
+    for i in range(100):
+        stock_price.append(stocks.calculate_next_price(i, stocks.invested_capital))
+    
+    time = [t for t in range(100)]
+    plt.plot(time, stock_price)
+    
+plt.show()
