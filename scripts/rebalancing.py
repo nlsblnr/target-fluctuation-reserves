059ae92e550ef0e6ec013b90a254728c608883d9
nlsblnr <nils.blunier@gmail.com>
2026-07-03 23:22:15 +0200

    added scripts folder, new script for the periodic rebalancing algorithm

---
+++ scripts/rebalancing.py
@@ -0,0 +1,5 @@
+'''
+Implement periodic rebalancing of the portfolio including investing new funds according to the investment strategy.
+
+This is important in order to improve a realistic portfolio behaviour.
+'''
